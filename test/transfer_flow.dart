d6c17b0eeedb5fd4c31dcae31c64aa88dc555fdf
Alex Felipe <alexfelipevieira@gmail.com>
2019-12-18 13:53:56 -0300

    adjusting the test for check the complete expected flow

--- test/transfer_flow.dart
+++ test/transfer_flow.dart
@@ -1,5 +1,7 @@
 import 'package:bytebank/main.dart';
 import 'package:bytebank/models/contact.dart';
+import 'package:bytebank/models/transaction.dart';
+import 'package:bytebank/screens/contacts_list.dart';
 import 'package:bytebank/screens/dashboard.dart';
 import 'package:bytebank/screens/transaction_form.dart';
 import 'package:flutter/material.dart';
@@ -28,20 +30,20 @@ void main() {
         featureItemMatcher(widget, 'Transfer', Icons.monetization_on));
     expect(featureTransfer, findsOneWidget);
 
-    when(mockContactDao.findAll())
-        .thenAnswer((_) async => [Contact(0, 'Alex', 1000)]);
+    final mockContact = Contact(0, 'Alex', 1000);
+
+    when(mockContactDao.findAll()).thenAnswer((_) async => [mockContact]);
 
     await tester.tap(featureTransfer);
 
     await tester.pumpAndSettle();
 
     final contact = find.byWidgetPredicate(
-          (widget) =>
-          ContactItemMatcher(
-            widget,
-            'Alex',
-            1000,
-          ),
+      (widget) => ContactItemMatcher(
+        widget,
+        'Alex',
+        1000,
+      ),
     );
     expect(contact, findsOneWidget);
     await tester.tap(contact);
@@ -52,7 +54,7 @@ void main() {
     expect(contactName, findsOneWidget);
 
     final contactAccountNumber =
-    find.byKey(transactionFormContactAccountNumberKey);
+        find.byKey(transactionFormContactAccountNumberKey);
     expect(contactAccountNumber, findsOneWidget);
 
     final valueField = find.byKey(transactionFormValueFieldKey);
@@ -84,11 +86,31 @@ void main() {
     final cancelButton = find.widgetWithText(FlatButton, 'Cancel');
     expect(cancelButton, findsOneWidget);
 
-    when(mockTransactionWebClient.save(any, any)).thenAnswer((_) async => any);
+    when(mockTransactionWebClient.save(any, any))
+        .thenAnswer((_) async => Transaction(
+              "",
+              200,
+              mockContact,
+            ));
 
     await tester.tap(confirmButton);
-    await tester.pumpAndSettle();
 
     verify(mockTransactionWebClient.save(any, any));
+
+    await tester.pumpAndSettle();
+
+    final successTitle = find.text('Success');
+    expect(successTitle, findsOneWidget);
+    final successIcon = find.byIcon(Icons.done);
+    expect(successIcon, findsOneWidget);
+    final successfulTransactionMessage = find.text('successful transaction');
+    expect(successfulTransactionMessage, findsOneWidget);
+    final okButton = find.widgetWithText(FlatButton, 'Ok');
+    expect(okButton, findsOneWidget);
+    await tester.tap(okButton);
+    await tester.pumpAndSettle();
+
+    final contactsList = find.byType(ContactsList);
+    expect(contactsList, findsOneWidget);
   });
 }
