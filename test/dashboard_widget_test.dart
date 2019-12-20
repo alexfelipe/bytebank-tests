d4022d748251cbfbefe1fd3aaf4aac9528b046b1
Alex Felipe <alexfelipevieira@gmail.com>
2019-12-20 12:24:18 -0300

    2.1

--- test/dashboard_widget_test.dart
+++ test/dashboard_widget_test.dart
@@ -2,6 +2,8 @@ import 'package:bytebank/screens/dashboard.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+int counter = 1;
+
 void main() {
   testWidgets('Should show the main image', (WidgetTester tester) async {
     await tester.pumpWidget(
@@ -10,11 +12,17 @@ void main() {
     final mainImage = find.byType(Image);
     expect(mainImage, findsOneWidget);
   });
-  testWidgets('Should show the first feature', (WidgetTester tester) async {
+  testWidgets('Should show the transfer feature', (WidgetTester tester) async {
     await tester.pumpWidget(
       MaterialApp(home: Dashboard()),
     );
-    final firstFeature = find.byType(FeatureItem);
-    expect(firstFeature, findsWidgets);
+    final transferFeature = find.byWidgetPredicate((Widget widget) {
+      if (widget is FeatureItem) {
+        return widget.name == 'Transfer' &&
+            widget.icon == Icons.monetization_on;
+      }
+      return false;
+    }, description: 'FeatureItem with text and icon');
+    expect(transferFeature, findsOneWidget);
   });
 }
